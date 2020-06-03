e8b8d9ac7c56acfa20e1becd7b1742f0234df83f
Yu Zhe <kelvinlim_98@live.com>
2020-06-03 14:05:26 +0800

    Added collection item component and styled the app

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1>{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.filter((item, index) => index < 4).map((item) => (
-        <div key={item.id}>{item.name}</div>
+      {items.filter((item, index) => index < 4).map(({ id, ...otherItemProps }) => (
+        <CollectionItem key={id} {...otherItemProps} />
       ))}
     </div>
   </div>
