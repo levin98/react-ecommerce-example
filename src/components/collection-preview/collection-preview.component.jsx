59eca8c5fedde06bae4ea21e292dd378264716fe
Yu Zhe <kelvinlim_98@live.com>
2020-06-03 13:53:30 +0800

    Added collection preview component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1>{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items.filter((item, index) => index < 4).map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
